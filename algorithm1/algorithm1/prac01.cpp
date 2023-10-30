6101e72eb0dbb2af7d7d3f549e0a6f1a01639719
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-10-31 01:19:39 +0900

    10/31_3

--- algorithm1/algorithm1/prac01.cpp
+++ algorithm1/algorithm1/prac01.cpp
@@ -6,6 +6,31 @@ using namespace std;
 
 int main()
 {
-	
+	int testcase;
+	cin >> testcase;
+
+	int answer = 0;
+	int A[10001] = { 0 };
+
+	for (int i = 1; i < 10001; i++) {
+		A[i] = rand() * 100;
+
+
+	}
+
+
+	for (int t = 1; t < testcase + 1; t++) {
+		int start, end;
+		cin >> start >> end;
+
+		for (int i = start; i <= end; i++) {
+			answer += A[i];
+
+
+		}
+
+		cout << testcase << " " << answer;
+
+	}
 
 }
